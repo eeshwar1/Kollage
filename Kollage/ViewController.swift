1cecb76f5b2fca9d306c2e13787b880ec5e548b7
Venky Venkatakrishnan <eeshwar1@gmail.com>
2019-09-09 21:59:48 -0500

    Initial Commit

---
+++ Kollage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kollage
+//
+//  Created by Venky Venkatakrishnan on 9/9/19.
+//  Copyright © 2019 Venky UL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
