02d296ffe4a75213080c9c2330fe6522ea3e07ad
Venky Venkatakrishnan <eeshwar1@gmail.com>
2019-09-15 20:23:06 -0500

    Initial Commit

---
+++ Kollage/VURoundRectView.swift
@@ -0,0 +1,23 @@
+//
+//  VURoundRectView.swift
+//  Kollage
+//
+//  Created by Venky Venkatakrishnan on 9/15/19.
+//  Copyright © 2019 Venky UL. All rights reserved.
+//
+
+
+import Cocoa
+
+@IBDesignable class VURoundedRectView: NSView {
+    
+    var radius : CGFloat = 10.0
+    
+    override func draw(_ dirtyRect: NSRect) {
+        
+        let path = NSBezierPath(roundedRect: NSInsetRect(bounds, radius, radius), xRadius: radius, yRadius: radius)
+        NSColor.white.set()
+        path.fill()
+    }
+}
+
